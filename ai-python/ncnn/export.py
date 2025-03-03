8cc5647cd3db104596426b8b5f8baf0675e10b72
hanruiyao <hanruiyao163@163.com>
2025-03-03 11:53:56 +0800

    2025-03-03 11:53:50

---
+++ ai-python/ncnn/export.py
@@ -0,0 +1,5 @@
+from ultralytics import YOLO
+
+from onnx import __version__, IR_VERSION
+from onnx.defs import onnx_opset_version
+print(f"onnx.__version__={__version__!r}, opset={onnx_opset_version()}, IR_VERSION={IR_VERSION}")       
