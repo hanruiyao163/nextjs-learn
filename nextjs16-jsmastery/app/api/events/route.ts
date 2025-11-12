e517ece38384aaf1e607024051d43bb012057bbf
duohelingdukele <hanruiyao163@163.com>
2025-11-12 18:14:35 +0800

    2025-06-23 18:13:42

---
+++ nextjs16-jsmastery/app/api/events/route.ts
@@ -0,0 +1,24 @@
+import Event from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+    let event;
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json({message: "Invalid JSON data format"}, {status: 400});
+    }
+
+    const createdEvent = await Event.create(event);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ message: "Event Creation Failed", error: e instanceof Error ? e.message : "Unknown" });
+  }
+
+}
+
