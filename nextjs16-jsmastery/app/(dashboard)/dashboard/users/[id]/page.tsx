376874b98d75be22097d95ababd17b8da8da484e
duohelingdukele <hanruiyao163@163.com>
2025-11-12 16:52:06 +0800

    2025-06-23 18:13:42

--- nextjs16-jsmastery/app/(dashboard)/dashboard/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page({params} : {params: Promise<{id: string}>}) {
-  const {id} = await params;
-
-  return (
-    <>
-      <h1>Showing UserDetails for ID: {id}</h1>
-    </>
-  );
-}
